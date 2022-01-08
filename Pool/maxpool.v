2eb10b2fca361c0a1564549a59e9a9760bb9a298
HugoAhoy <zhjd3302@outlook.com>
2022-01-08 17:11:22 +0800

    fix some bug in maxpool module

--- Pool/maxpool.v
+++ Pool/maxpool.v
@@ -6,7 +6,7 @@ module maxpool
     PATCH,
     RESULT
 );
-    localparam DATAWIDTH = 64;
+    localparam DATAWIDTH = 16;
     input CLK;
     input rst_n;
     input  wire [4*DATAWIDTH-1:0] PATCH;
@@ -14,6 +14,8 @@ module maxpool
     
     reg [DATAWIDTH-1:0] PIXELS[3:0];
     reg [DATAWIDTH-1:0] RES;
+    reg [DATAWIDTH-1:0] TEMP_RES1;
+    reg [DATAWIDTH-1:0] TEMP_RES2;
 
     assign RESULT=RES;
 
@@ -23,21 +25,27 @@ module maxpool
 
     always @(*) begin
         if(PIXELS[0] < PIXELS[1])begin
-            PIXELS[0] = PIXELS[1];
+            TEMP_RES1 = PIXELS[1];
+        end
+        else begin
+            TEMP_RES1 = PIXELS[0];
         end
     end
     always @(*) begin
         if(PIXELS[2] < PIXELS[3])begin
-            PIXELS[2] = PIXELS[3];
-        end        
+            TEMP_RES2 = PIXELS[3];
+        end
+        else begin
+            TEMP_RES2 = PIXELS[2];
+        end
     end
 
     always @(*) begin
-        if(PIXELS[0] < PIXELS[2])begin
-            RES = PIXELS[2];
+        if(TEMP_RES1 < TEMP_RES2)begin
+            RES = TEMP_RES2;
         end
         else begin
-            RES = PIXELS[2];
+            RES = TEMP_RES1;
         end
     end
 
